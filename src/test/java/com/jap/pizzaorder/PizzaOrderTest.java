4a3bcf7aff8e0fb1c6cdb7467b5bc13691f68182
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-08-02 12:53:49 +0530

    challenge1

--- src/test/java/com/jap/pizzaorder/PizzaOrderTest.java
+++ src/test/java/com/jap/pizzaorder/PizzaOrderTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class PizzaOrderTest {
+    PizzaOrder object = new PizzaOrder();
     PizzaOrder pizzaOrder = null;
     @Before
     public void setUp(){
@@ -21,46 +22,55 @@ public class PizzaOrderTest {
     // Write th logic inside the test case methods
     @Test
     public void givenNoOfPizzaAndPriceOfPizzaCalculateTotalPriceSuccess(){
-
+        float actual = object.calculatePriceOfPizza(1,10.0f);
+        assertEquals(10.0f,actual,1);
     }
     @Test
     public void givenNoOfGarlicBreadAndPriceOfGarlicBreadCalculateTotalPriceSuccess(){
-
+        float actual = object.calculatePriceOfGarlicBread(1,5.99f);
+        assertEquals(5.99f,actual,1);
     }
     @Test
     public void givenNoOfBeveragesAndPriceOfBeverageCalculateTotalPriceSuccess(){
-
+        float actual = object.calculatePriceOfBeverage(1,1.99f);
+        assertEquals(1.99f,actual,1);
     }
     @Test
     public void givenNoOfPizzaAndPriceOfPizzaCalculateTotalPriceFailure(){
-
-    }
+        float actual = object.calculatePriceOfPizza(1,5.99f);
+        assertNotEquals(4.56,actual,1);}
     @Test
     public void givenNoOfGarlicBreadAndPriceOfGarlicBreadCalculateTotalPriceFailure(){
-
+        float actual = object.calculatePriceOfGarlicBread(1,5.99f);
+        assertNotEquals(2.45f,actual,1);
     }
     @Test
     public void givenNoOfBeveragesAndPriceOfBeverageCalculateTotalPriceFailure(){
-
+        float actual = object.calculatePriceOfBeverage(1,5.99f);
+        assertNotEquals(3.67f,actual,1);
     }
     @Test
     public void givenSizeofPizzaReturnPrice(){
-
+        float actual = object.calculatePriceOfPizza(1,10.0f);
+        assertEquals(10.0f,actual,1);
 
     }
     @Test
     public void givenPriceOfItemsCalculateTotalBillAmount(){
-
+        float actual = object.calculateTotalBill(10.0f,5.99f,1.99f);
+        assertEquals(17.98f,actual,1);
     }
 
     @Test
     public void givenTotalBillAmountGreaterThan50CalculateDiscount(){
-
+        float actual = object.calculateDiscountAndReturnBillAmount(100.0f);
+        assertEquals(90.0f,actual,1);
 
     }
     @Test
     public void givenTotalBillLessThan50AmountCalculateDiscount(){
-
+        float actual = object.calculateTotalBill(1,5,3);
+        assertEquals(9,actual,1);
 
     }
 }
